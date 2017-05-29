49c2a56f96bdaeeee039be6409ec8aa8bec41c5c
reuben453 <reuben@vacationlabs.com>
2017-05-29 18:03:20 +0530

    Use mailtrap for local mails
    * Add .env to gitignore
    * Add dotenv-rails and rest-client gems to development and test groups

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,22 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+
+  require 'rest-client'
+  require 'json'
+
+  response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}"
+
+  first_inbox = JSON.parse(response)[0] # get first inbox
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :user_name => first_inbox['username'],
+    :password => first_inbox['password'],
+    :address => first_inbox['domain'],
+    :domain => first_inbox['domain'],
+    :port => first_inbox['smtp_ports'][0],
+    :authentication => :plain
+  }
+
 end
