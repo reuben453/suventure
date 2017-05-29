ba626e59eada9eab03eb073439b646d2bc1a5273
reuben453 <reuben@vacationlabs.com>
2017-05-29 17:14:39 +0530

    Add tests for username format
    * Remove fixtures for messages and users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,40 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "username validations @" do
+    user = User.create(username: "asf@fg.com", email: "some@thing.com", password: "password")
+    assert_equal(1, user.errors.messages.length)
+    assert_equal(["is invalid"], user.errors.messages[:username])
+  end
+
+  test "username validations %" do
+    user = User.create(username: "asf%fg", email: "some@thing.com", password: "password")
+    assert_equal(1, user.errors.messages.length)
+    assert_equal(["is invalid"], user.errors.messages[:username])
+  end
+
+  test "username validations &" do
+    user = User.create(username: "asf&fg", email: "some@thing.com", password: "password")
+    assert_equal(1, user.errors.messages.length)
+    assert_equal(["is invalid"], user.errors.messages[:username])
+  end
+
+  test "username validations *" do
+    user = User.create(username: "asf*fg", email: "some@thing.com", password: "password")
+    assert_equal(1, user.errors.messages.length)
+    assert_equal(["is invalid"], user.errors.messages[:username])
+  end
+
+  test "username validations |" do
+    user = User.create(username: "asf|fg", email: "some@thing.com", password: "password")
+    assert_equal(1, user.errors.messages.length)
+    assert_equal(["is invalid"], user.errors.messages[:username])
+  end
+
+  test "username validations allowed chars" do
+    user = User.create(username: "asf_.AS56fg", email: "some@thing.com", password: "password")
+    assert_equal(0, user.errors.messages.length)
+  end
+
 end
