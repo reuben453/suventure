74ed8faec39ccb1f89c5463eb8c230bfdad822f1
reuben453 <reuben@vacationlabs.com>
2017-05-30 18:08:28 +0530

    Correct the api return data format so it more closely resembles the requested format

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ApiController < ActionController::API
   acts_as_token_authentication_handler_for User, fallback: :exception
 
   def index
-    render :json => current_user
+    render :json => {:user => current_user}
   end
 
 end
